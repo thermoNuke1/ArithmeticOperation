e0a493285e2785ad7bb136ac54c1ae26b918066c
ThermoNuke1 <ca372717@gmail.com>
2025-04-04 18:44:42 -0400

    less tests

--- src/test/java/soen/tutorial/ArithmeticOperationsTest.java
+++ src/test/java/soen/tutorial/ArithmeticOperationsTest.java
@@ -6,26 +6,24 @@ import static org.junit.Assert.assertEquals;
 
 public class ArithmeticOperationsTest {
 
-
-	@Test
-	public void testAdd()
-	{
-
-		ArithmeticOperations operations = new ArithmeticOperations();
-		Integer actual = operations.addOrSub(2, 6);
-		Integer expected = 8;
-		assertEquals(expected, actual);
-
-	}
     @Test
-    public void testSub()
-    {
+    public void testAdd() {
 
         ArithmeticOperations operations = new ArithmeticOperations();
-        Integer actual = operations.addOrSub(6, 2);
-        Integer expected = 4;
+        Integer actual = operations.addOrSub(2, 6);
+        Integer expected = 8;
         assertEquals(expected, actual);
 
     }
+    // @Test
+    // public void testSub()
+    // {
+
+    // ArithmeticOperations operations = new ArithmeticOperations();
+    // Integer actual = operations.addOrSub(6, 2);
+    // Integer expected = 4;
+    // assertEquals(expected, actual);
+
+    // }
 
 }
